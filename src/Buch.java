18f0de9ae487b35c500f88e33268ccf95d1290d2
Emil <emweiss@tsn.at>
2024-09-25 20:26:03 +0200

    Buch Klasse neu hinzugefügt

--- src/Buch.java
+++ src/Buch.java
@@ -1,24 +1,24 @@
 
 public class Buch extends Medium{
-	
-	private String isbn;
 
-	public Buch(String titel, double wert, String standort, String isbn) {
-		super(titel, wert, standort);
-		this.isbn = isbn;
-	}
+    private String isbn;
 
-	public String getIsbn() {
-		return isbn;
-	}
+    public Buch(String titel, double wert, String standort, String isbn) {
+        super(titel, wert, standort);
+        this.isbn = isbn;
+    }
 
-	public void setIsbn(String isbn) {
-		this.isbn = isbn;
-	}
-	
-	public void anzeigen()//�berschreiben der Mutterklassenmethode
-	{
-		super.anzeigen();
-		System.out.println("[Buch] : ISBN -> " + this.getIsbn());
-	}
+    public String getIsbn() {
+        return isbn;
+    }
+
+    public void setIsbn(String isbn) {
+        this.isbn = isbn;
+    }
+
+    public void anzeigen()//�berschreiben der Mutterklassenmethode
+    {
+        super.anzeigen();
+        System.out.println("[Buch] : ISBN -> " + this.getIsbn());
+    }
 }
